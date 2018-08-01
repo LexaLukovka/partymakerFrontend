e968f32d51ea99b3b55d84eea256ba619b6507c1
luker <lukerlexa@gmail.com>
2018-08-01 11:11:37 +0300

    delete auth action in connector from /parties

--- src/components/@parties/connector.js
+++ src/components/@parties/connector.js
@@ -1,6 +1,5 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import * as authAction from '../../redux/auth/action'
 import * as partyActions from '../../redux/party/list/action'
 import * as party from '../../redux/party/single/action'
 import * as like from '../../redux/party/like/action'
@@ -9,7 +8,6 @@ const initMapStateToProps = store => store.party.listReducer
 
 const initMapDispatchToProps = dispatch => ({
   actions: {
-    auth: bindActionCreators(authAction, dispatch),
     party: bindActionCreators(party, dispatch),
     parties: bindActionCreators(partyActions, dispatch),
     like: bindActionCreators(like, dispatch),
