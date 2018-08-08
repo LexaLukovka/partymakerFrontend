c3f9cddf1094db57ba8c1f1f93a46379ce4c1213
luker <lukerlexa@gmail.com>
2018-08-08 16:02:44 +0300

    add formik for EmailScene in settings

--- src/components/@settings/@email/EmailScene.jsx
+++ src/components/@settings/@email/EmailScene.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { object } from 'prop-types'
+import { func, object } from 'prop-types'
 import { withStyles, Typography, TextField, Button } from '@material-ui/core'
 import Helper from '../Helper'
 import connector from '../connector'
+import formik from './formik'
 
 const styles = theme => ({
   root: {
@@ -25,30 +26,52 @@ class EmailScene extends React.Component {
     this.props.actions.header.resetTitle()
   }
 
+  hasError = (fieldName) => {
+    const { errors, touched } = this.props
+    return (!!errors[fieldName] && touched[fieldName])
+  }
+
+  showHelperError = (fieldName) => {
+    const { errors, touched } = this.props
+    return (touched[fieldName] && errors[fieldName])
+  }
+
+
   render() {
-    const { classes, user } = this.props
+    const { classes, values, handleSubmit, handleChange, handleBlur } = this.props
     return (
-      <div className={classes.root}>
+      <form onSubmit={handleSubmit} className={classes.root}>
         <div className={classes.input}>
           <Typography variant="subheading">Email</Typography>
           <TextField
             fullWidth
             name="email"
             placeholder="Email"
-            defaultValue={user.email}
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={this.hasError('email')}
+            helperText={this.showHelperError('email')}
           />
         </div>
         <Helper>Ваша почта будет видна всем всем людям на вашей вечеринке</Helper>
-        <Button variant="raised" color="primary" className={classes.button}> Сохранить </Button>
-      </div>
+        <Button variant="raised" color="primary" className={classes.button} type="submit">
+          Сохранить
+        </Button>
+      </form>
     )
   }
 }
 
 EmailScene.propTypes = {
   classes: object.isRequired,
-  user: object.isRequired,
   actions: object.isRequired,
+  values: object.isRequired,
+  errors: object.isRequired,
+  touched: object.isRequired,
+  handleSubmit: func.isRequired,
+  handleChange: func.isRequired,
+  handleBlur: func.isRequired,
 }
 
-export default withStyles(styles)(connector(EmailScene))
+export default formik(connector(withStyles(styles)(EmailScene)))
