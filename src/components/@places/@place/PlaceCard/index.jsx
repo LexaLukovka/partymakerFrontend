4643499be64f3cdb820bc5ab6e6f7a2c0ea92288
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2018-08-10 17:44:55 +0300

    add PlaceCard

--- src/components/@places/@place/PlaceCard/index.jsx
+++ src/components/@places/@place/PlaceCard/index.jsx
@@ -1,75 +1,82 @@
+/* eslint-disable no-console */
 import React from 'react'
 import { object, number, string, array, shape } from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import { Paper, ListItemSecondaryAction, ListItemText, List, Typography, Button } from '@material-ui/core'
-import ListItem from './ListItem'
-import connector from '../connector'
-import shortTitle from 'utils/shortTitle'
+import ReactStars from 'react-stars'
+import { Typography, Button } from '@material-ui/core'
+import { AddIcon, LocationIcon } from 'mdi-react'
 
 const styles = theme => ({
   root: {
-    marginBottom: 15,
+    position: 'relative',
     padding: 15,
+    paddingTop: 25,
+    background: theme.palette.primary.main,
+    color: 'white',
   },
 
-  amount: {
-    color: theme.palette.primary.main,
-    width: '45%',
-    textAlign: 'right',
+  rating: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingBottom: 10,
   },
-  status: {
-    width: 17,
-    height: 17,
-    background: theme.palette.primary.main,
-    borderRadius: '50%',
+  rating_number: {
+    fontSize: '18px',
+    padding: '0 5px',
+  },
+  location: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingBottom: 10,
+  },
+  floatingButton: {
+    position: 'absolute',
+    top: -27,
+    right: 15,
   },
 })
 
+const ratingChanged = (newRating) => {
+  console.log(newRating)
+}
+
+
 const PlaceCard = ({ classes, place }) =>
-  <Paper className={classes.root}>
-    <Typography align="center" variant="title">{place.title}</Typography>
-    <List>
-      <ListItem>
-        <div className={classes.status} />
-        <ListItemText primary={place.status} />
-        <ListItemSecondaryAction className={classes.amount}>
-          {place.admin.name}
-        </ListItemSecondaryAction>
-      </ListItem>
-      {
-        place.amount &&
-        <ListItem>
-          <ListItemText primary="Скидываться" />
-          <ListItemSecondaryAction>
-            {`${place.amount} грн`}
-          </ListItemSecondaryAction>
-        </ListItem>
-      }
-      <ListItem>
-        <ListItemText primary="Район" />
-        <ListItemSecondaryAction>
-          {place.address.district}
-        </ListItemSecondaryAction>
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Адрес" secondary={shortTitle(place.address.address)} />
-        <ListItemSecondaryAction>
-          <a href={`http://www.google.com/maps/?q=${place.address.address}`}>
-            <Button variant="contained" size="small" color="primary">Показать на карте</Button>
-          </a>
-        </ListItemSecondaryAction>
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Описание" secondary={place.description} />
-      </ListItem>
-      <ListItem>
-        <ListItemText
-          primary="Telegram"
-          secondary={<a href={place.telegram_url}>{`${place.telegram_url.substring(0, 40)}...`}</a>}
-        />
-      </ListItem>
-    </List>
-  </Paper>
+  <section className={classes.root}>
+    <Button variant="fab" color="default" className={classes.floatingButton} aria-label="Add">
+      <AddIcon />
+    </Button>
+    <div className={classes.location}>
+      <LocationIcon />
+      <Typography color="inherit" variant="subheading">
+        {place.address.address}
+      </Typography>
+    </div>
+    <Typography
+      color="inherit"
+      variant="title"
+      className={classes.title}
+    >
+      {place.title}
+    </Typography>
+
+    <div className={classes.rating}>
+      <Typography color="inherit" className={classes.rating_number}> 3.7 </Typography>
+      <ReactStars
+        count={5}
+        value={3.7}
+        onChange={ratingChanged}
+        size={32}
+        color2="#ffd700"
+      />
+    </div>
+
+    <Typography color="inherit">
+      Очень хорошее место что бы бесплатно отдохнуть на природе.
+      Пожарить шашлыков, весело провести время на майские
+      праздники.
+    </Typography>
+  </section>
 
 PlaceCard.propTypes = {
   classes: object.isRequired,
@@ -84,4 +91,4 @@ PlaceCard.propTypes = {
   }).isRequired,
 }
 
-export default withStyles(styles)(connector(PlaceCard))
+export default withStyles(styles)(PlaceCard)
--- src/components/@places/@place/PlaceScene.jsx
+++ src/components/@places/@place/PlaceScene.jsx
@@ -2,8 +2,6 @@
 import React from 'react'
 import { arrayOf, bool, object } from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import { CSSTransition } from 'react-transition-group'
-import { Button } from '@material-ui/core'
 import isEmpty from 'lodash/isEmpty'
 import Loading from 'components/Loading'
 import NotFound from 'components/NotFound'
@@ -24,18 +22,6 @@ const styles = (theme) => ({
     marginTop: 20,
     textAlign: 'center',
   },
-  papers: {
-    position: 'absolute',
-    top: 200,
-    left: 0,
-    right: 0,
-    margin: 9,
-  },
-  paperse: {
-    marginTop: 20,
-    marginLeft: 9,
-    marginRight: 9,
-  },
   container: {
     display: 'flex',
   },
@@ -51,10 +37,6 @@ const styles = (theme) => ({
 })
 
 class PlaceScene extends React.Component {
-  state = {
-    checked: false,
-  }
-
   componentDidMount() {
     const { actions, match } = this.props
     actions.place.show(match.params.id)
@@ -66,29 +48,16 @@ class PlaceScene extends React.Component {
     actions.header.setIcon('menu')
   }
 
-  handleClick = (check) => {
-    this.setState({
-      checked: check === this.state.checked && !check,
-    })
-  }
 
   render() {
     const { classes, loading, place } = this.props
     if (loading) return <Loading />
     if (isEmpty(place)) return <NotFound />
-    const { checked } = this.state
 
     return (
       <div className={classes.root}>
-        <div onClick={() => this.handleClick(checked)}>
-          <Carousel pictures={place.pictures} />
-        </div>
-        <CSSTransition in={checked} timeout={500} classNames="star">
-          <div className={!checked ? classes.papers : classes.paperse}>
-            <PlaceCard place={place} />
-            <Button variant="raised" size="large" fullWidth color="primary">Я ПОЙДУ</Button>
-          </div>
-        </CSSTransition>
+        <Carousel pictures={place.pictures} />
+        <PlaceCard place={place} />
       </div>
     )
   }
