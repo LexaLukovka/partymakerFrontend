268cdd1e0d7d38e9a03ee9b3812cfde90fc05a18
pavliha <kostyk.pavel.09@gmail.com>
2018-09-02 18:13:20 +0300

    fix Service Worker

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -6,9 +6,10 @@ const merge = require('webpack-merge')
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index.js'),
-  ],
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+    sw: path.resolve(__dirname, 'src/sw.js'),
+  },
   target: 'web',
   plugins: [
     new BundleAnalyzerPlugin(),
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,9 @@ const merge = require('webpack-merge')
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index.js'),
-  ],
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+    sw: path.resolve(__dirname, 'src/sw.js'),
+  },
   target: 'web',
 })
