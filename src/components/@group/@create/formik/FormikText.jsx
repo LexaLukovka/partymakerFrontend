4447b7231cedfc82ccd4a31e7c68cb0f3bd95590
luker <lukerlexa@gmail.com>
2018-09-29 14:12:19 +0300

    fix create group

--- src/components/@group/@create/formik/FormikText.jsx
+++ src/components/@group/@create/formik/FormikText.jsx
@@ -2,23 +2,18 @@ import React from 'react'
 import { func, number, object, shape, string } from 'prop-types'
 import { TextField } from '@material-ui/core'
 
-const FormikText = ({ field, form, rows, rowsMax, ...props }) =>
+const FormikText = ({ field, form, ...props }) =>
   <TextField
     {...props}
     {...field}
     InputLabelProps={{ shrink: true }}
     fullWidth
-    multiline
-    rows={rows}
-    rowsMax={rowsMax}
     margin="dense"
     error={(form.submitCount > 0) && !!form.errors[field.name]}
     helperText={(form.submitCount > 0) && form.errors[field.name]}
   />
 
 FormikText.propTypes = {
-  rows: number,
-  rowsMax: number,
   field: shape({
     name: string,
     value: string,
@@ -29,11 +24,6 @@ FormikText.propTypes = {
     errors: object,
     submitCount: number,
   }).isRequired,
-}
-
-FormikText.defaultProps = {
-  rows: null,
-  rowsMax: null,
 }
 
 export default FormikText
